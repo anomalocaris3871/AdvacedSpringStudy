0b1e08d386bafaad5a2bf569b94478c5d3da69b4
lee <anomalocaris3871@gmail.com>
2022-03-15 23:31:33 +0900

    add v3 test

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,29 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+    FieldLogStatus trace = new FieldLogStatus();
+
+    @Test
+    void begin_end_test() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_end_exception() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}

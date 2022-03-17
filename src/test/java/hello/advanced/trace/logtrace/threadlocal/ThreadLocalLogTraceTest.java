5994d3eb3c7cdca784ae3bbe3bda173e266c0335
lee <anomalocaris3871@gmail.com>
2022-03-17 21:52:28 +0900

    add ThreadLocal and test code

---
+++ src/test/java/hello/advanced/trace/logtrace/threadlocal/ThreadLocalLogTraceTest.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.logtrace.threadlocal;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.FieldLogStatus;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
+import org.junit.jupiter.api.Test;
+
+
+class ThreadLocalLogTraceTest {
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+    @Test
+    void begin_end_test() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_end_exception() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
